870a86bd1a79473c4c1c6d1eb7b47887c42d3a9c
Adaephon-GH <adaephon@gmx.net>
2014-03-28 21:30:27 +0100

    Do not use exit in config-files, duh!

--- .zsh/zshrc.d/ubuntu.zsh
+++ .zsh/zshrc.d/ubuntu.zsh
@@ -1,11 +1,10 @@
 # System specific code for Ubuntu
 
 # exit if this is not an ubuntu
-if [[ ! -s '/etc/os-release' ]] || ! grep -q ubuntu '/etc/os-release'; then
-    exit 0
+if [[ -s '/etc/os-release' ]] && grep -q ubuntu '/etc/os-release'; then
+    if [[ -s '/etc/zsh_command_not_found' ]]; then
+        source '/etc/zsh_command_not_found'
+    fi
 fi
 
-if [[ -s '/etc/zsh_command_not_found' ]]; then
-    source '/etc/zsh_command_not_found'
-fi
 
