cbf5ddb76a419260ed8597184974b8d1b99246f5
Adaephon-GH <adaephon@gmx.net>
2014-04-16 12:26:04 +0200

    Handle numpad keys

--- .zsh/zshrc.d/bindkey.zsh
+++ .zsh/zshrc.d/bindkey.zsh
@@ -22,8 +22,28 @@ bindkey "^[OF" end-of-line
 bindkey "^[[F" end-of-line
 bindkey "^[[8~" end-of-line
 
-# KP Enter
-bindkey "^[OM" accept-line
+# KP
+# 0 . Enter
+bindkey -s "^[Op" "0"
+bindkey -s "^[Ol" "."
+bindkey -s "^[OM" "^M"
+# 1 2 3
+bindkey -s "^[Oq" "1"
+bindkey -s "^[Or" "2"
+bindkey -s "^[Os" "3"
+# 4 5 6
+bindkey -s "^[Ot" "4"
+bindkey -s "^[Ou" "5"
+bindkey -s "^[Ov" "6"
+# 7 8 9
+bindkey -s "^[Ow" "7"
+bindkey -s "^[Ox" "8"
+bindkey -s "^[Oy" "9"
+# + -  * /
+bindkey -s "^[Ok" "+"
+bindkey -s "^[Om" "-"
+bindkey -s "^[Oj" "*"
+bindkey -s "^[Oo" "/"
 
 # Del
 bindkey "^[[3~" delete-char
