2ac5838ed4a83f13d54a217502c44ea8bf2ba111
Adaephon-GH <adaephon@gmx.net>
2014-07-15 22:22:02 +0200

    add dynamic named dir for temporary directories

--- .zsh/zshrc.d/dyn_named_dir.zsh
+++ .zsh/zshrc.d/dyn_named_dir.zsh
@@ -28,6 +28,39 @@ __config.git() {
     return 0
 }
 
+__tempdirs() {
+    emulate -L zsh
+    setopt extendedglob
+    setopt nullglob
+    local -a match mbegin mend
+    if [[ $1 = d ]]; then
+        if [[ $2 = (#b)(/tmp/foo/)([^/]##)* ]]; then
+            typeset -ga reply
+            reply=(foo:$match[2] $(( ${#match[1]} + ${#match[2]} )) )
+        else
+            return 1
+        fi
+    elif [[ $1 = n ]]; then
+        [[ $2 != (#b)foo:(?*) ]] && return 1
+        typeset -ga reply
+        reply=("/tmp/foo/${match[1]}")
+    elif [[ $1 = c ]]; then
+        local expl
+        local -a dirs
+        dirs=(/tmp/foo/*(/:t))
+        for (( i=1; i<=$#dirs; i++ )); do
+            dirs[$i]=foo:${dirs[$i]}
+        done
+        _wanted dynamic-dirs expl 'dynamic directory' compadd -S\] -a dirs
+        return
+    else
+        return 1
+    fi
+    return 0
+}
+
+
+
 typeset -a zsh_directory_name_functions
-zsh_directory_name_functions=(__config.git)
+zsh_directory_name_functions=(__config.git __tempdirs)
 
