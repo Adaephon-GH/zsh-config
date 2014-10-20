1af3374741552c2b28eb3aefa6e4a52dba8f8040
Adaephon-GH <adaephon@gmx.net>
2014-10-20 15:40:20 +0200

    add alias for temporary ssh connections

--- .zsh/zshrc.d/aliases.zsh
+++ .zsh/zshrc.d/aliases.zsh
@@ -39,3 +39,5 @@ alias cgit="git --work-tree='$HOME' --git-dir=.git"
 alias -g GG='| grep'
 
 alias -g NPK='-o PubkeyAuthentication=no'
+
+alias ssh-temp='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
