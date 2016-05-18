71f0a346cdcc4ea9e49ed68e25049da2a37779a7
Adaephon-GH <adaephon@gmx.net>
2016-05-19 00:29:24 +0200

    Move PACMAN configuration to arch.zsh

--- .zsh/zshrc.d/arch.zsh
+++ .zsh/zshrc.d/arch.zsh
@@ -1,5 +1,10 @@
 # System specific code for Arch
 
+# Use powerpill as download manager for pacman, if available
+if [[ -x /usr/bin/powerpill ]]; then
+    export PACMAN=/usr/bin/powerpill
+fi
+
 # only run this on Arch
 if [[ -s '/etc/os-release' ]] && grep -q 'Arch Linux' '/etc/os-release'; then
     if [[ -s '/usr/share/doc/pkgfile/command-not-found.zsh' ]]; then
--- .zshenv
+++ .zshenv
@@ -1,7 +1,3 @@
-# Use powerpill as download manager for pacman, if available
-if [[ -x /usr/bin/powerpill ]]; then
-    export PACMAN=/usr/bin/powerpill
-fi
 
 # Set up pyenv (https://github.com/yyuu/pyenv)
 PYENV_ROOT="${HOME}/local/opt/pyenv"
