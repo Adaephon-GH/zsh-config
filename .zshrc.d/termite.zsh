4bec6961eb3f9b02aadc2ca6a76eb5fc868842a9
Adaephon-GH <adaephon@gmx.net>
2016-08-31 09:26:43 +0200

    Simplify check for vte.sh

--- .zshrc.d/termite.zsh
+++ .zshrc.d/termite.zsh
@@ -1,8 +1,6 @@
 VTE_SH=/etc/profile.de/vte.sh
-if [[ $TERM == xterm-termite ]]; then
-    if [[ -e $VTE_SH ]]; then
-        . $VTE_SH
-        whence __vte_osc7 > /dev/null && __vte_osc7
-    fi
+if [[ $TERM == xterm-termite && -e $VTE_SH ]]; then
+    . $VTE_SH
+    whence __vte_osc7 > /dev/null && __vte_osc7
 fi
 unset VTE_SH
