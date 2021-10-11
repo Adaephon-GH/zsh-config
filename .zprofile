68210a2aad591b3363470fc9b8c57ab66e0f078a
Adaephon-GH <adaephon@gmx.net>
2021-10-11 17:07:55 +0200

    Update pyenv initialization

--- .zprofile
+++ .zprofile
@@ -1,3 +1,12 @@
+# Set up pyenv
+_pyenv_root="${HOME}/local/opt/pyenv"
+if [[ -d ${_pyenv_root} && ! -e /tmp/nopyenv ]]; then
+    export PYENV_ROOT=$_pyenv_root
+    PATH="${PYENV_ROOT}/bin:${PATH}"
+    eval "$(${PYENV_ROOT}/bin/pyenv init --path)"
+fi
+unset _pyenv_root
+
 # automatically start X when logging in on VT1
 if [[ -z $DISPLAY && $XDG_VTNR -eq 1 && ! -e /tmp/.X0-lock ]]; then
     exec startx
--- .zshrc.d/99-pyenv.zsh
+++ .zshrc.d/99-pyenv.zsh
@@ -1,9 +1,5 @@
-# Set up pyenv (https://github.com/yyuu/pyenv)
-PYENV_ROOT="${HOME}/local/opt/pyenv"
-if [[ -d ${PYENV_ROOT} && ! -e /tmp/nopyenv ]]; then
-    export PYENV_ROOT
-    export PATH="${PYENV_ROOT}/bin:${PATH}"
-    eval "$(pyenv init -)"
-else
-    unset PYENV_ROOT
+# Set up shell functionality for pyenv 
+# (https://github.com/pyenv/pyenv)
+if [[ -n ${PYENV_ROOT} ]]; then
+    eval "$(${PYENV_ROOT}/bin/pyenv init -)"
 fi
